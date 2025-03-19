a6b68e6e972d439830354a4156e9db20c0ab6bce
gumphp <gumphp@qq.com>
2025-03-19 22:49:38 +0800

    Update ConfigProvider.php

--- src/ConfigProvider.php
+++ src/ConfigProvider.php
@@ -37,15 +37,15 @@ class ConfigProvider
             // 与 commands 类似
             'listeners' => [],
             // 组件默认配置文件，即执行命令后会把 source 的对应的文件复制为 destination 对应的的文件
-            'publish' => [
-                [
-                    'id' => 'config',
-                    'description' => 'description of this config file.', // 描述
-                    // 建议默认配置放在 publish 文件夹中，文件命名和组件名称相同
-                    'source' => __DIR__ . '/../publish/file.php',  // 对应的配置文件路径
-                    'destination' => BASE_PATH . '/config/autoload/file.php', // 复制为这个路径下的该文件
-                ],
-            ],
+            // 'publish' => [
+            //     [
+            //         'id' => 'config',
+            //         'description' => 'description of this config file.', // 描述
+            //         // 建议默认配置放在 publish 文件夹中，文件命名和组件名称相同
+            //         'source' => __DIR__ . '/../publish/file.php',  // 对应的配置文件路径
+            //         'destination' => BASE_PATH . '/config/autoload/file.php', // 复制为这个路径下的该文件
+            //     ],
+            // ],
             // 亦可继续定义其它配置，最终都会合并到与 ConfigInterface 对应的配置储存器中
         ];
     }
